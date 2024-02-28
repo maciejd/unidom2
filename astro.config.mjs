68b00eedf79e11ecb6e22c5e44ee46e0ca7c89b7
maciejd <dobrowolski6@gmail.com>
2024-02-28 13:49:06 -0500

    fixed domain name and removed remaining references to github.io base

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://maciejd.github.io',
-    base: '/unidom2',
+    site: 'https://unidomy.pl'
 });
